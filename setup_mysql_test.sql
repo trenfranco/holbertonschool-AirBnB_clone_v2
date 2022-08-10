546ba33acb52a0fc666822017332d1f1f950f607
trenfranco <trenfranco@gmail.com>
2022-08-10 01:48:51 -0700

    syntax fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
--- test
--- DATABASE CREATION
+-- Creates database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
