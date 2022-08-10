bdd31413627473689c194c6b63941a01df3d5fab
trenfranco <trenfranco@gmail.com>
2022-08-10 13:39:22 -0700

    ex 7

--- models/user.py
+++ models/user.py
@@ -3,6 +3,10 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
+import models
+import sqlalchemy
+from os import getenv
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -11,3 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
