ba911acfff199eb0ebf8460aee1fcf91923a69c4
trenfranco <trenfranco@gmail.com>
2022-08-29 18:38:28 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,6 @@ echo "Test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-
-sed -i "/server_name _/a location \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
+sed -i "/listen 80 default_server/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 service nginx restart
