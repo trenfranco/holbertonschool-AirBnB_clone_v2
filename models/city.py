cd7bedc202eeedf49bccd3f0b87d1124d92ca05d
trenfranco <trenfranco@gmail.com>
2022-09-07 18:18:44 -0700

    some fixes

--- models/city.py
+++ models/city.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
+import os
 
 
 class City(BaseModel, Base):
-    """city class """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities",
-                            cascade="all, delete-orphan")
+    """ The city class, contains state ID and name """
+    type_storage = os.getenv("HBNB_TYPE_STORAGE")
+    __tablename__ = "cities"
+
+    if type_storage == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False,)
+        places = relationship("Place", cascade="all,delete", backref='cities')
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -13,8 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship('City', backref="state", cascade="all, \
-                              delete-orphan")
+        cities = relationship('City', backref="state", cascade="all, delete")
 
     if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
